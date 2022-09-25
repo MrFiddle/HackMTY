f19034b3185c9a08362b9e2f170e6f5ddee29393
Juan Pablo <jppd0657@gmail.com>
2022-09-25 03:29:59 -0500

    commit de no se que

--- shoptwin/src/App.js
+++ shoptwin/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ItemComponent from './components/ItemComponent';
+import PriceContainer from './components/PriceContainer';
+import Home from './components/Home';
 
 function App() {
 
   return (
-      <div>Shop twin</div>
+      <div>
+        <Home/>
+      </div>
   );
 }
 
